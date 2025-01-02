fc679740857d86f4aab19373a2282c16b5e9096a
Hoà Phạm Khắc <hoapham@MacBookPro.lan>
2025-01-03 01:17:21 +0700

    init 3

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:t_store/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/device/device_utility.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
+import '../../../../utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,7 +24,33 @@ class HomeScreen extends StatelessWidget {
           children: [
             TPrimaryHeaderContainer(
               child: Column(
-                children: [THomeAppBar()],
+                children: [
+                  const THomeAppBar(),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  TSearchContainer(
+                    text: 'Search in Store',
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        TSectionHeading(
+                          title: 'Popular Categories',
+                          showActionButton: false,
+                        ),
+                        const SizedBox(
+                          height: TSizes.spaceBtwItems,
+                        ),
+                        THomeCategories()
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
