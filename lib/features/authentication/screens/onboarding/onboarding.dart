3f8eb4e630b905e8d5a589bb98770cc74c5d24e3
Hoà Phạm Khắc <hoapham@Hoas-MacBook-Pro.local>
2025-01-02 12:29:52 +0700

    first commit

---
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/utils.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:t_store/features/authentication/controllers.onboarding/onboarding_controller.dart';
+import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
+import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/device/device_utility.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(OnboardingController());
+    return Scaffold(
+      body: Stack(
+        children: [
+          /// Horizontal Scrollable Pages
+          PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
+            children: [
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+              )
+            ],
+          ),
+
+          /// Skip Button
+          const OnBoardingSkip(),
+
+          /// Dot Navigation SmootPageIndicator
+          const OnBoardingDotNavigation(),
+
+          /// Circular Button
+          const OnBoardingNextButton()
+        ],
+      ),
+    );
+  }
+}
+
+class OnBoardingDotNavigation extends StatelessWidget {
+  const OnBoardingDotNavigation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = OnboardingController.instance;
+
+    return Positioned(
+      bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
+      left: TSizes.defaultSpace,
+      child: SmoothPageIndicator(
+        controller: PageController(),
+        onDotClicked: controller.dotNavigationClick,
+        count: 3,
+        effect: const ExpandingDotsEffect(
+          activeDotColor: TColors.dark,
+          dotHeight: 6,
+        ),
+      ),
+    );
+  }
+}
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(
+      right: TSizes.defaultSpace,
+      bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+      child: ElevatedButton(
+          onPressed: () {
+            OnboardingController.instance.nextPage();
+          },
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            backgroundColor: dark ? TColors.primary : Colors.black,
+            foregroundColor: Colors.white,
+          ),
+          child: const Icon(
+            Iconsax.arrow_right_3,
+          )),
+    );
+  }
+}
