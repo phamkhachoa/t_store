5515652d9da37bffef08152d36ab54e7d5940bde
Hoà Phạm Khắc <hoapham@MacBookPro.lan>
2025-01-10 20:43:35 +0700

    new

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
-import 'package:t_store/common/widgets/icons/t_circular_icon.dart';
-import 'package:t_store/common/widgets/images/t_rounded_image.dart';
 import 'package:t_store/common/widgets/texts/product_price_text.dart';
-import 'package:t_store/common/widgets/texts/product_title_text.dart';
-import 'package:t_store/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
-import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
 import '../../../../common/widgets/products/cart/add_remove_button.dart';
 import '../../../../common/widgets/products/cart/cart_item.dart';
-import '../../../../utils/constants/colors.dart';
-import '../../../../utils/helpers/helper_functions.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -27,41 +19,43 @@ class CartScreen extends StatelessWidget {
           style: Theme.of(context).textTheme.headlineSmall,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(TSizes.defaultSpace),
-          child: ListView.separated(
-            // scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemCount: 4,
-            separatorBuilder: (_, __) =>
-                const SizedBox(height: TSizes.spaceBtwSections),
-            itemBuilder: (_, index) => Column(
-              children: [
-                TCartItem(),
-                SizedBox(
-                  height: TSizes.spaceBtwItems,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        const SizedBox(width: 70),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ListView.separated(
+          // scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: 10,
+          separatorBuilder: (_, __) =>
+              const SizedBox(height: TSizes.spaceBtwSections),
+          itemBuilder: (_, index) => Column(
+            children: [
+              TCartItem(),
+              SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const SizedBox(width: 70),
 
-                        /// Add Remove Buttons
-                        TProductQuantityWithAddRemoveButton(),
-                      ],
-                    ),
+                      /// Add Remove Buttons
+                      TProductQuantityWithAddRemoveButton(),
+                    ],
+                  ),
 
-                    /// -- Product total price
-                    TProductPriceText(price: '256', maxLines: 1),
-                  ],
-                ),
-              ],
-            ),
+                  /// -- Product total price
+                  TProductPriceText(price: '256', maxLines: 1),
+                ],
+              ),
+            ],
           ),
         ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ElevatedButton(onPressed: () {}, child: Text('Checkout 250 \$')),
       ),
     );
   }
